c289c6d7db0f0c10d8b1803fa62eca67f4b9dba8
Jeff Brunson <jeff.a.brunson@gmail.com>
2016-08-15 23:19:59 -0600

    api setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import _ from 'lodash';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import YTSearch from 'youtube-search-api';
+import VideoDetail from './VideoDetail';
 
-export default class App extends Component {
+const API_KEY = "AIzaSyDwpg7z0bjbSVSehYTh59nrGY2r5QprYp8";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    }; 
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render() {
+    //throttle search on keypress
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
     );
   }
 }
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       loader: 'babel'
     }]
   },
+  devtool: 'source-map',
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
